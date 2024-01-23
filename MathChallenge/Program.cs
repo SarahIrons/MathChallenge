fd6f4f7c03a46623f7d40e98495d22ab0bcdf35c
SarahIrons <sarahirons11@gmail.com>
2024-01-23 14:08:58 -0500

    Add project files.

---
+++ MathChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add two numbers and display the output in the console.
+            Console.WriteLine("What is 3 + 8?");
+            int num1 = 3;
+            int num2 = 8;
+            int total = num1 + num2;
+            Console.WriteLine(total);
+
+
+            //Subtract two numbers and display the output in the console.
+
+            Console.WriteLine("What is 12-10?");
+            int var1 = 12;
+            int var2 = 10;
+            int difference = var1 - var2;
+            Console.WriteLine(difference);
+
+            //Multiply two numbers and display the output in the console.
+
+            Console.WriteLine("What is 16 * 2?");
+            int no1 = 16;
+            int no2 = 2;
+            int product = no1 * no2;
+            Console.WriteLine(product);
+
+            //Divide two numbers and display the output in the console.
+
+            Console.WriteLine("What is 60/4?");
+            int fig1 = 60;
+            int fig2 = 4;
+            int quotient = fig1 / fig2;
+            Console.WriteLine(quotient);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
